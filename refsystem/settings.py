8e18532f40a065814dbdb1aa085c02f2a30090a3
Shauryaman Sawai <sawaisushil@gmail.com>
2026-09-18 21:53:05 +0530

    Fix Django static files

--- refsystem/settings.py
+++ refsystem/settings.py
@@ -146,6 +146,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
